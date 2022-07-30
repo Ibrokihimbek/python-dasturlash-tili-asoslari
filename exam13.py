fe2db15169eedccd11d620458b592113cff78763
Ibrokhimbek <ibrohim9902@gmail.com>
2022-07-30 13:42:05 +0500

    Eng ko'p va eng uzun so'zni chiqazish

---
+++ exam13.py
@@ -0,0 +1,13 @@
+# # 2.	String ma'lumotlarini input orqali kiriting va ushbu stringdagi eng ko'p uchraydigan
+# #so'zni va eng uzun so'zni chiqaring.
+
+# text = input().replace('.',' ')
+# a, b, c = 1, '', set(text.split())
+# d = max([text.count(i) for i in c])
+# print("Eng ko'p qatnashgan so'z: ", end = '')
+# for _ in c:
+#     if d == text.count(_):
+#         print(_, end = " ")
+#     if len(_) > a:
+#         a,b = len(_), _
+# print(f"\nEng uzun so'z: {b}")
